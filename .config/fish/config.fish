008a88a98903255543c9d61b45d3d6d2c0d0cb16
Gregory-F <guegre1979@netcourrier.com>
2019-04-06 09:16:05 +0200

    reworked the line breaking

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,4 +2,5 @@ set PATH $PATH $HOME/bin
 
 source ~/.config/fish/abrev
 
-set fish_greeting " Go Fish !!!"alias config='/usr/bin/git --git-dir=/home/greg/.cfg/ --work-tree=/home/greg'
+set fish_greeting " Go Fish !!!"
+alias config='/usr/bin/git --git-dir=/home/greg/.cfg/ --work-tree=/home/greg'
