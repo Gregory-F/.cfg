5ced2d6334821fdba21f8dad8e0e4864617f7073
Gregory-F <guegre1979@netcourrier.com>
2020-01-26 15:16:16 +0100

    update 26/01/2020

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,5 +2,11 @@ set PATH $PATH $HOME/bin
 
 source ~/.config/fish/abrev
 
+export CLOUDSDK_PYTHON=/usr/bin/python2
+
 set fish_greeting " Go Fish !!!"
 alias config='/usr/bin/git --git-dir=/home/greg/.cfg/ --work-tree=/home/greg'
+
+
+#setenv EDITOR /usr/bin/vim
+#set -Ux EDITOR /usr/bin/vim 
