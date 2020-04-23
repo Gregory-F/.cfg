dead7b330c12250231e571dc045fbf679ef943bd
Gregory-F <guegre1979@netcourrier.com>
2020-04-23 17:28:41 +0200

    added config allias in fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,3 +10,4 @@ alias config='/usr/bin/git --git-dir=/home/greg/.cfg/ --work-tree=/home/greg'
 
 #setenv EDITOR /usr/bin/vim
 #set -Ux EDITOR /usr/bin/vim 
+alias config='/usr/bin/git --git-dir=/home/greg/.cfg/ --work-tree=/home/greg'
