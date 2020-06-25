03ecc9b700e43a759777fb7f9e574d8d9bd2ab52
Gregory-F <guegre1979@netcourrier.com>
2020-06-25 17:36:39 +0200

    edited fish configs files

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,9 +2,9 @@ set PATH $PATH $HOME/bin
 
 source ~/.config/fish/abrev
 
-export CLOUDSDK_PYTHON=/usr/bin/python2
-
 set fish_greeting " Go Fish !!!"
+neofetch
+
 alias config='/usr/bin/git --git-dir=/home/greg/.cfg/ --work-tree=/home/greg'
 
 
