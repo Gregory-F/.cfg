92b429621fe86f99ae702451be9c99b7222ab085
Gregory-F <guegre1979@netcourrier.com>
2020-09-11 09:29:46 +0200

    remove unesseray comments

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,8 +6,3 @@ set fish_greeting " Go Fish !!!"
 neofetch
 
 alias config='/usr/bin/git --git-dir=/home/greg/.cfg/ --work-tree=/home/greg'
-
-
-#setenv EDITOR /usr/bin/vim
-#set -Ux EDITOR /usr/bin/vim 
-alias config='/usr/bin/git --git-dir=/home/greg/.cfg/ --work-tree=/home/greg'
