5c1ef75f3c43d7ec13ab5bd34f6239243e6d2705
Gregory-F <guegre1979@netcourrier.com>
2021-03-16 19:13:03 +0100

    commented out the greeting

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,7 @@ set PATH $PATH $HOME/bin
 
 source ~/.config/fish/abrev
 
-set fish_greeting " Go Fish !!!"
+#set fish_greeting " Go Fish !!!"
 neofetch
 
 alias config='/usr/bin/git --git-dir=/home/greg/.cfg/ --work-tree=/home/greg'
