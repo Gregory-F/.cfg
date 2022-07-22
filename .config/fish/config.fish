84e061b473e2ac25da693f38d0437513bb88226f
greg <guegre1979@netcourrier.com>
2022-07-22 17:01:02 +0200

    changed all path to '/home/greg' instead of hard coded

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,11 +5,11 @@ source ~/.config/fish/abrev
 set fish_greeting " Go Fish !!!"
 neofetch --ascii ~/.config/neofetch/ascii
 
-alias config='/usr/bin/git --git-dir=/home/greg/.cfg/ --work-tree=/home/greg'
+alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 
 export IPYTHONDIR="$XDG_CONFIG_HOME"/jupyter
 export JUPYTER_CONFIG_DIR="$XDG_CONFIG_HOME"/jupyter
-set GNUPGHOME ~/.config/gnupg/
+#set GNUPGHOME $HOME/config/gnupg/
 
 ############
 #set -Ux variable value
